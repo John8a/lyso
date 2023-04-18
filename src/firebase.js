a07be85a7eb1d1b660905b77fdde130e2b6da279
John v. Hardenberg <john.hardenberg123@gmail.com>
2023-04-18 15:31:46 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLmUhGCfIsFuLnNBvlcA_oLf9dw71xwFM",
+  apiKey: "AIzaSyCLmUhGCfIsFuLnNBvlcZ7sLf9dw71xwFM",
   authDomain: "lyso-b9bb9.firebaseapp.com",
   projectId: "lyso-b9bb9",
   storageBucket: "lyso-b9bb9.appspot.com",
